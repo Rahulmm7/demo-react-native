c8765d5e4044e83a32b3cfefb92ce47bb7839893
Rahul <rahulmanohar007@gmail.com>
2023-06-14 10:38:59 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  SafeAreaView,
+  StyleSheet
+}
+  from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello world!!!!!1</Text>
+      <Text>
+        this is home page!!!!
+      </Text>
+    </SafeAreaView>
+  )
+}
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#000'
+  }
+})
+
+export default App;
---
+++ AppPro.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const AppPro = () => {
+    return (
+        <View style={styles.conatiner}><Text>Helloooooooo</Text></View>
+    )
+}
+const styles = StyleSheet.create({
+    conatiner: {
+        flex: 1,
+        alignItems: 'center'
+    }
+})
+
+export default AppPro
